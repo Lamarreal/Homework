ba0099285404a139fba4e00a079316602cd294e4
maksb <maksb@MAKSPC>
2024-05-19 10:57:38 +0300

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿// First 
+
+float combinedNumber = 0;
+float MaxDigits = 4;
+
+for (float i = 0; i < MaxDigits; i++) // we will receive four numbers from user and combine them to one
+{
+    Console.Write($"Enter number {i} : ");
+    string? Input = Console.ReadLine();
+
+    if (Input != null)
+    {
+        int toInt = Convert.ToInt32(Input);
+
+        float pow = MathF.Pow(10, MaxDigits);
+
+        combinedNumber += toInt * pow;
+    }
+
+}
+
+Console.WriteLine(combinedNumber.ToString());
