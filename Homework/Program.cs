82cd4205ea921d21481cb4e56ab9569187038af6
maksb <maksb@MAKSPC>
2024-05-20 08:52:19 +0300

    finished V.0.01

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -3,20 +3,16 @@
 float combinedNumber = 0;
 float MaxDigits = 4;
 
-for (float i = 0; i < MaxDigits; i++) // we will receive four numbers from user and combine them to one
+for (float i = 1; i < MaxDigits + 1; i++) // we will receive four numbers from user and combine them to one
 {
     Console.Write($"Enter number {i} : ");
     string? Input = Console.ReadLine();
 
     if (Input != null)
     {
-        int toInt = Convert.ToInt32(Input);
-
-        float pow = MathF.Pow(10, MaxDigits);
-
-        combinedNumber += toInt * pow;
+        int toInt = Convert.ToInt32(Input); // converting input message to int
+        float pow = MathF.Pow(10, MaxDigits - i); // power function for math calculations
+        combinedNumber += toInt * pow; // calculating final number
     }
-
 }
-
-Console.WriteLine(combinedNumber.ToString());
+Console.WriteLine("Your number is :" +  combinedNumber.ToString());
